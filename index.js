215101b87823505e418ab61fdd10c8943d0ccec4
Nicolas Graziano <nicolas.graziano@engie.com>
2019-11-04 19:39:57 +0100

    WIP

--- index.js
+++ index.js
@@ -7,25 +7,144 @@ const commandLineArgs = require("command-line-args");
  */
 const optionDefinitions = [
   { name: "verbose", alias: "v", type: Boolean },
-  { name: "mqtturl", type: String, multiple: true, defaultOption: true }
+  { name: "mqtturl", type: String, multiple: false, defaultOption: true },
+  { name: "mqttprefix", type: String, defaultValue: "shellies" },
+  { name: "homeassistantprefix", type: String, defaultValue: "homeassistant" }
 ];
 
 const args = commandLineArgs(optionDefinitions);
+/**
+ *
+ * @param {{id:string, type:string}} device
+ */
+function getHomeAssistantDevice(device) {
+  return {
+    identifiers: ["shelly", device.id],
+    manufacturer: "Shelly",
+    model: device.type,
+    name: device.id
+  };
+}
 
-shellies.on("discover", device => {
-  // a new device has been discovered
-  console.log("Discovered device with ID", device.id, "and type", device.type);
+/**
+ *
+ * @param {import("async-mqtt").AsyncMqttClient} client
+ * @param {{id:string, type:string}} device
+ */
+async function addToHomeAssistantDiscover(client, device) {
+  if (!args.homeassistantprefix) {
+    return;
+  }
+  try {
+    const prefix = `${args.homeassistantprefix}/`;
+    const id = `/shellies/${device.id}/`;
+    switch (device.type) {
+      case "SHPLG-S":
+        await client.publish(
+          prefix + "sensor" + id + "config",
+          JSON.stringify({
+            name: "SH-" + device.id + "-power",
+            state_topic: `${args.mqttprefix}/${device.id}/powerMeter0`,
+            unit_of_measurement: "W",
+            device: getHomeAssistantDevice(device),
+            device_class: "power",
+            unique_id: "SH-" + device.id + "-power"
+          }),
+          {
+            retain: true,
+            qos: 0
+          }
+        );
+        await client.publish(
+          prefix + "switch" + id + "config",
+          JSON.stringify({
+            name: "SH-" + device.id + "-state",
+            unique_id: "SH-" + device.id + "-state",
+            state_topic: `${args.mqttprefix}/${device.id}/relay0`,
+            command_topic: `${args.mqttprefix}/${device.id}/relay0/SET`,
+            state_on: "true",
+            state_off: "false",
+            device: getHomeAssistantDevice(device)
+          }),
+          {
+            retain: true,
+            qos: 0
+          }
+        );
+        //
+        break;
 
-  device.on("change", (prop, newValue, oldValue) => {
-    // a property on the device has changed
-    console.log(device.id, prop, "changed from", oldValue, "to", newValue);
+      default:
+        break;
+    }
+  } catch (error) {
+    console.error("Erreur during publish of auto discovery", error);
+  }
+}
+
+async function start() {
+  const client = await MQTT.connectAsync(
+    args.mqtturl,
+    { connectTimeout: 10000 },
+    true
+  );
+  console.log("MQTT connected ?", client.connected);
+
+  shellies.on("discover", device => {
+    // a new device has been discovered
+    console.log(
+      "Discovered device with ID",
+      device.id,
+      "and type",
+      device.type
+    );
+    client
+      .publish(`${args.mqttprefix}/${device.id}/state`, "online", {
+        qos: 0,
+        retain: true
+      })
+      .catch(error => console.error("Error during publish", error));
+
+    client
+      .publish(`${args.mqttprefix}/${device.id}/type`, device.type, {
+        qos: 0,
+        retain: true
+      })
+      .catch(error => console.error("Error during publish", error));
+
+    addToHomeAssistantDiscover(client, device);
+
+    device.on("change", (prop, newValue, oldValue) => {
+      // a property on the device has changed
+      console.log(device.id, prop, "changed from", oldValue, "to", newValue);
+      client
+        .publish(
+          `${args.mqttprefix}/${device.id}/${prop}`,
+          JSON.stringify(newValue),
+          {
+            qos: 0,
+            retain: true
+          }
+        )
+        .catch(error => console.error("Error during publish", error));
+    });
+
+    device.on("offline", () => {
+      // the device went offline
+      console.log("Device with ID", device.id, "went offline");
+      client
+        .publish(`${args.mqttprefix}/${device.id}/state`, "offline", {
+          qos: 0,
+          retain: true
+        })
+        .catch(error => console.error("Error during publish", error));
+    });
   });
+  // start discovering devices and listening for status updates
+  await shellies.start();
+  console.log("Start listening shellies");
+}
 
-  device.on("offline", () => {
-    // the device went offline
-    console.log("Device with ID", device.id, "went offline");
-  });
+start().catch(error => {
+  console.error("Fatal error :", error);
 });
-
-// start discovering devices and listening for status updates
-shellies.start();
